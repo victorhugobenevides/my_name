df182ad67fb23c79b67f93ec0febfe83cc042e0d
Victor Benvides <victorkoto@gmail.com>
2024-09-12 11:58:50 -0300

    wip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.itbenevides.myname"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
